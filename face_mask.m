f32ff307dbe819ad771766fe1bafddb38b2516fa
cindykhuong <cindykhuong123@hotmail.com>
2022-11-16 17:49:59 +0100

    Skin detection

--- face_mask.m
+++ face_mask.m
@@ -1,5 +1,8 @@
-function [binaryImg] = face_mask(inputImg)
+function [binaryImg] = faceMask(inputImg)
+%funkar inte helt, den får med bakgrunden i vissa bilder
+
 %to binary image
+%binaryImg= zeros(256);
 binaryImg = zeros(size(inputImg, 1), size(inputImg,2));
 
 %Convert image from RGB to YCbCr
@@ -10,32 +13,29 @@ Y = YCbCr_img(:,:,1);
 Cb = YCbCr_img(:,:,2);
 Cr = YCbCr_img(:,:,3);
 
-% 
-% Y = inputImg(:,:,1);
-% Cb = inputImg(:,:,2);
-% Cr = inputImg(:,:,3);
 
-%Från lecture
+%Från lecture, 77 ≤ Cb ≤ 127 snd 133 ≤ Cr ≤ 173, Y illumination
 [row, col] = find(Cb>=85 & Cr>=135 & Y >= 80);
-
 
 %Mark the skin to white
 for n=1:size(row)
     binaryImg(row(n), col(n))= 1;
 end
 
-%Morphological operations to get rid of holes in the face and some of the
+%Morphological operations gets rid of the holes in the face and some of the
 %background
 
 %binaryImg = inputImg;
-binaryImg = imopen(binaryImg, strel('disk',12));
-binaryImg = imclose(binaryImg, strel('disk',12)); 
 
-binaryImg = imdilate(binaryImg, strel('disk',12));
-binaryImg = imdilate(binaryImg, strel('disk',12));
-binaryImg = imerode(binaryImg, strel('disk',12));
-binaryImg = imerode(binaryImg, strel('disk',12));
+se = strel('disk',12);
+binaryImg = imopen(binaryImg, se);
+binaryImg = imclose(binaryImg, se); 
+ 
+binaryImg = imdilate(binaryImg, se);
+binaryImg = imdilate(binaryImg, se);
+binaryImg = imerode(binaryImg, se);
+binaryImg = imerode(binaryImg, se);
 
-
+imshow(binaryImg);
 end
 
